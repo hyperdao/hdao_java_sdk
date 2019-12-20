ec96d5b7a30886a939cc99e4acc637a9f96b945d
sleeprich <sleeprich@pm.me>
2019-12-20 18:02:38 +0800

    first commit

---
+++ src/main/java/hdao/sdk/HdaoClient.java
@@ -0,0 +1,9 @@
+package hdao.sdk;
+
+import cash.hx.hxjava.client.NodeClient;
+
+public class HdaoClient extends NodeClient {
+    public HdaoClient(String nodeRpcEndpoint) {
+        super(nodeRpcEndpoint);
+    }
+}
---
+++ src/main/java/hdao/sdk/HdaoTransactionBuilder.java
@@ -0,0 +1,10 @@
+package hdao.sdk;
+
+import cash.hx.hxjava.exceptions.TransactionException;
+import cash.hx.hxjava.transaction.Transaction;
+
+public class HdaoTransactionBuilder {
+    public Transaction transferCdc(String cdcContractAddress, String cdcId, String toAddr) throws TransactionException {
+        return null; // TODO
+    }
+}
